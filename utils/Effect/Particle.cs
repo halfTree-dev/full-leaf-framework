206675448e9069c3814afa53d9275e865a87e9ae
HelloWorld486 <272926141@qq.com>
2023-11-25 11:35:53 +0800

    写了关于粒子生成器的json

--- utils/Effect/Particle.cs
+++ utils/Effect/Particle.cs
@@ -3,3 +3,18 @@ Particle 即粒子效果，
 这里描述一个单独的粒子，
 结合粒子控制器，可以做到对粒子效果的很好控制
 */
+
+using full_leaf_framework.Visual;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace full_leaf_framework.Effect;
+
+/// <summary>
+/// 单个粒子
+/// </summary>
+public class Particle : Drawable
+{
+    public Particle(AnimatedSprite currentAnimation, Vector2 pos, Vector2 anchorPoint, float sizeScale, float angle = 0, SpriteEffects effects = SpriteEffects.None, int layer = 10) : base(currentAnimation, pos, anchorPoint, sizeScale, angle, effects, layer) {
+    }
+}
---
+++ utils/Effect/ParticleController.cs
@@ -0,0 +1,6 @@
+/*
+ParticleController 粒子效果控制器
+它负责生成、处理粒子，将绘制信息传入摄像机
+*/
+
+using Microsoft.Xna.Framework;
